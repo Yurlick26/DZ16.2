e12f4ad20af3b5a3c0dfe956c8a5957feedc24ca
Vladyslav <vladurlov17@gmail.com>
2025-10-21 09:26:41 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine(); // читаем строку от пользователя
+
+        if (double.TryParse(input, out double number))
+        {
+            Console.WriteLine($"You entered a number: {number}");
+        }
+        else
+        {
+            Console.WriteLine("Error: The entered value is not a number!");
+        }
+    }
+}
